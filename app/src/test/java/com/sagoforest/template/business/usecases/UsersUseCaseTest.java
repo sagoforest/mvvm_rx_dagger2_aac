bdcca6d627427d2b99f23ab775740a68d490d094
Andrew Hanna <andrew.hanna@novoengineering.com>
2018-05-15 12:38:32 -0700

    Updates

---
+++ app/src/test/java/com/sagoforest/template/business/usecases/UsersUseCaseTest.java
@@ -0,0 +1,23 @@
+
+
+
+package com.sagoforest.template.business.usecases;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UsersUseCaseTest {
+
+    @Test
+    public void getUsers() {
+    }
+
+    @Test
+    public void addNew() {
+    }
+
+    @Test
+    public void clearAll() {
+    }
+}
